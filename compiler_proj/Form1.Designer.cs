da5f62b371b325563e7145733ef79dc33ac31bba
Ahmedabdalaziz <ahmedabdalziz.1886@gmail.com>
2024-12-24 19:18:36 +0200

    Add project files.

---
+++ compiler_proj/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace compiler_proj
+{
+    partial class IDE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.inputCode = new System.Windows.Forms.TextBox();
+            this.outputAnalyze = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.parserButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button.Location = new System.Drawing.Point(187, 553);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(154, 115);
+            this.button.TabIndex = 2;
+            this.button.Text = "Scanner";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.analyze);
+            // 
+            // inputCode
+            // 
+            this.inputCode.AccessibleDescription = "";
+            this.inputCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputCode.Location = new System.Drawing.Point(5, 1);
+            this.inputCode.Multiline = true;
+            this.inputCode.Name = "inputCode";
+            this.inputCode.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputCode.Size = new System.Drawing.Size(587, 535);
+            this.inputCode.TabIndex = 3;
+            this.inputCode.Tag = "";
+            this.inputCode.TextChanged += new System.EventHandler(this.input_code);
+            // 
+            // outputAnalyze
+            // 
+            this.outputAnalyze.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.outputAnalyze.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputAnalyze.Location = new System.Drawing.Point(598, 1);
+            this.outputAnalyze.Multiline = true;
+            this.outputAnalyze.Name = "outputAnalyze";
+            this.outputAnalyze.ReadOnly = true;
+            this.outputAnalyze.Size = new System.Drawing.Size(585, 673);
+            this.outputAnalyze.TabIndex = 4;
+            this.outputAnalyze.Tag = "";
+            this.outputAnalyze.Text = "Your Output";
+            this.outputAnalyze.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.outputAnalyze.TextChanged += new System.EventHandler(this.output_analyze);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.clearButton.Location = new System.Drawing.Point(12, 553);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(154, 115);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click_1);
+            // 
+            // parserButton
+            // 
+            this.parserButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.parserButton.Location = new System.Drawing.Point(375, 553);
+            this.parserButton.Name = "parserButton";
+            this.parserButton.Size = new System.Drawing.Size(154, 115);
+            this.parserButton.TabIndex = 6;
+            this.parserButton.Text = "Parser";
+            this.parserButton.UseVisualStyleBackColor = false;
+            this.parserButton.Click += new System.EventHandler(this.parserButton_Click);
+            // 
+            // IDE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1195, 680);
+            this.Controls.Add(this.parserButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.outputAnalyze);
+            this.Controls.Add(this.inputCode);
+            this.Controls.Add(this.button);
+            this.Name = "IDE";
+            this.Text = "IDE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox inputCode;
+        private System.Windows.Forms.TextBox outputAnalyze;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button parserButton;
+    }
+}
+
---
+++ compiler_proj/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace compiler_proj
+{
+    public partial class IDE : Form
+    {
+        private static readonly HashSet<string> Keywords = new HashSet<string>
+        {
+            "اذا", "طالما", "متغير", "اطبع", "ادخل", "اخرج", "حرف", "رقم", "نص"
+        };
+
+        private static readonly Regex IdentifierRegex = new Regex(@"^[a-zA-Z_]\w*$");
+        private static readonly Regex NumberRegex = new Regex(@"^\d+(\.\d+)?$");
+        private static readonly Regex WhiteSpaceRegex = new Regex(@"^\s+$");
+        private static readonly Regex OperatorRegex = new Regex(@"^[\+\-\*/%=<>!&|]+$");
+        private static readonly Regex PunctuationRegex = new Regex(@"^[\(\)\{\}\[\],;.]$");
+        private static readonly Regex SemiColumnRegex = new Regex(@"^[;\.]$");
+        private static readonly Regex StringRegex = new Regex("^\".*\"$");
+        private static readonly Regex CommentRegex = new Regex(@"^//.*?$|/\*[\s\S]*?\*/$");
+
+        public IDE()
+        {
+            InitializeComponent();
+        }
+
+        private void input_code(object sender, EventArgs e)
+        {
+        }
+
+        private void output_analyze(object sender, EventArgs e)
+        {
+        }
+
+        private void analyze(object sender, EventArgs e)
+        {
+            string sourceCode = inputCode.Text;
+            outputAnalyze.Clear();
+
+            var tokenPattern =
+                @"(\b(اذا|طالما|متغير|اطبع|ادخل|اخرج|حرف|رقم|نص)\b|\d+(\.\d+)?|""[^""]*""|//.*?$|/\*[\s\S]*?\*/|[\+\-\*/%=<>!&|]+|[\(\)\{\}\[\],;.]|[a-zA-Z_]\w*)";
+
+            var matches = Regex.Matches(sourceCode, tokenPattern, RegexOptions.Multiline);
+
+            foreach (Match match in matches)
+            {
+                string token = match.Value;
+
+                if (Keywords.Contains(token))
+                {
+                    if (token == "اطبع")
+                    {
+                        outputAnalyze.AppendText($"Token: '{token}', Type: Print Function{Environment.NewLine}");
+                    }
+                    else if (token == "ادخل")
+                    {
+                        outputAnalyze.AppendText($"Token: '{token}', Type: Input Function{Environment.NewLine}");
+                    }
+                    else if (token == "اخرج")
+                    {
+                        outputAnalyze.AppendText($"Token: '{token}', Type: Output Function{Environment.NewLine}");
+                    }
+                    else if (token == "حرف")
+                    {
+                        outputAnalyze.AppendText($"Token: '{token}', Type: Character Type{Environment.NewLine}");
+                    }
+                    else if (token == "رقم")
+                    {
+                        outputAnalyze.AppendText($"Token: '{token}', Type: Number Type{Environment.NewLine}");
+                    }
+                    else if (token == "نص")
+                    {
+                        outputAnalyze.AppendText($"Token: '{token}', Type: String Type{Environment.NewLine}");
+                    }
+                    else
+                    {
+                        outputAnalyze.AppendText($"Token: '{token}', Type: Keyword{Environment.NewLine}");
+                    }
+                }
+                else if (IdentifierRegex.IsMatch(token))
+                {
+                    outputAnalyze.AppendText($"Token: '{token}', Type: Identifier{Environment.NewLine}");
+                }
+                else if (NumberRegex.IsMatch(token))
+                {
+                    outputAnalyze.AppendText($"Token: '{token}', Type: Number{Environment.NewLine}");
+                }
+                else if (OperatorRegex.IsMatch(token))
+                {
+                    outputAnalyze.AppendText($"Token: '{token}', Type: Operator{Environment.NewLine}");
+                }
+                else if (PunctuationRegex.IsMatch(token))
+                {
+                    outputAnalyze.AppendText($"Token: '{token}', Type: Punctuation Mark{Environment.NewLine}");
+                }
+                else if (StringRegex.IsMatch(token))
+                {
+                    outputAnalyze.AppendText($"Token: '{token}', Type: String{Environment.NewLine}");
+                }
+                else if (SemiColumnRegex.IsMatch(token))
+                {
+                    outputAnalyze.AppendText($"Token: '{token}', Type: Semicolon{Environment.NewLine}");
+                }
+                else
+                {
+                    outputAnalyze.AppendText($"Token: '{token}', Type: Unknown{Environment.NewLine}");
+                }
+            }
+        }
+
+        private void clearButton_Click_1(object sender, EventArgs e)
+        {
+            inputCode.Clear();
+            outputAnalyze.Clear();
+        }
+
+        private void parserButton_Click(object sender, EventArgs e)
+        {
+            outputAnalyze.Clear();
+
+            string sourceCode = inputCode.Text;
+            var tokens = Tokenize(sourceCode); 
+            string error = Parse(tokens);
+
+            if (string.IsNullOrEmpty(error))
+            {
+                outputAnalyze.AppendText("Parsing tree was successful! No errors." + Environment.NewLine);
+            }
+            else
+            {
+                outputAnalyze.AppendText("Parse error: " + error + Environment.NewLine);
+            }
+        }
+
+        private List<string> Tokenize(string sourceCode)
+        {
+            var tokenPattern =
+                @"(\b(اذا|طالما|متغير|اطبع|ادخل|اخرج|حرف|رقم|نص)\b|\d+(\.\d+)?|""[^""]*""|//.*?$|/\*[\s\S]*?\*/|[\+\-\*/%=<>!&|]+|[\(\)\{\}\[\],;.]|[a-zA-Z_]\w*)";
+            var matches = Regex.Matches(sourceCode, tokenPattern, RegexOptions.Multiline);
+            var tokens = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                tokens.Add(match.Value);
+            }
+
+            return tokens;
+        }
+
+        private string Parse(List<string> tokens)
+        {
+            int index = 0;
+
+            string Expect(string expectedToken)
+            {
+                if (index < tokens.Count && tokens[index] == expectedToken)
+                {
+                    index++;
+                    return null;
+                }
+                else
+                {
+                    return $"Expected '{expectedToken}' but found '{(index < tokens.Count ? tokens[index] : "end of file")}'";
+                }
+            }
+
+            string ParseExpression()
+            {
+                var error = ParseTerm();
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                while (index < tokens.Count && OperatorRegex.IsMatch(tokens[index]))
+                {
+                    index++; 
+                    error = ParseTerm();
+                    if (!string.IsNullOrEmpty(error)) return error;
+                }
+
+                return null;
+            }
+
+            string ParseTerm()
+            {
+                if (index < tokens.Count && (NumberRegex.IsMatch(tokens[index]) || StringRegex.IsMatch(tokens[index]) || IdentifierRegex.IsMatch(tokens[index])))
+                {
+                    index++;
+                    return null;
+                }
+                else if (index < tokens.Count && tokens[index] == "(")
+                {
+                    index++;
+                    var error = ParseExpression();
+                    if (!string.IsNullOrEmpty(error)) return error;
+
+                    error = Expect(")");
+                    if (!string.IsNullOrEmpty(error)) return error;
+
+                    return null;
+                }
+                return $"Expected a valid expression but found '{(index < tokens.Count ? tokens[index] : "end of file")}'";
+            }
+
+            string ParseStatement()
+            {
+                if (index < tokens.Count)
+                {
+                    if (tokens[index] == "اذا")
+                        return ParseIf();
+                    else if (tokens[index] == "طالما")
+                        return ParseLoop();
+                    else if (tokens[index] == "متغير")
+                        return ParseVariableDeclaration();
+                    else if (tokens[index] == "اطبع")
+                        return ParsePrintStatement();
+                    else
+                        return $"Unsupported statement: '{tokens[index]}'";
+                }
+
+                return $"Unexpected end of file.";
+            }
+
+            string ParseIf()
+            {
+                var error = Expect("اذا");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = Expect("(");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = ParseExpression();
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = Expect(")");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                return ParseBody();
+            }
+
+            string ParseLoop()
+            {
+                var error = Expect("طالما");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = Expect("(");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = ParseExpression(); 
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = Expect(")");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                return ParseBody(); 
+            }
+
+            string ParseVariableDeclaration()
+            {
+                var error = Expect("متغير");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = ExpectIdentifier();
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = Expect("=");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = ParseExpression();
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                return Expect(";");
+            }
+
+            string ParsePrintStatement()
+            {
+                var error = Expect("اطبع");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = Expect("(");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = ParseExpression(); 
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = Expect(")");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                error = Expect(";");
+                if (!string.IsNullOrEmpty(error)) return error;
+
+                return null;
+            }
+
+            string ExpectIdentifier()
+            {
+                if (index < tokens.Count && IdentifierRegex.IsMatch(tokens[index]))
+                {
+                    index++;
+                    return null;
+                }
+                return $"Expected a valid identifier but found '{(index < tokens.Count ? tokens[index] : "end of file")}'";
+            }
+
+            string ParseBody()
+            {
+                if (index < tokens.Count && tokens[index] == "{")
+                {
+                    index++;
+                    while (index < tokens.Count && tokens[index] != "}")
+                    {
+                        var error = ParseStatement();
+                        if (!string.IsNullOrEmpty(error)) return error;
+                    }
+
+                    if (index < tokens.Count && tokens[index] == "}")
+                    {
+                        index++; 
+                        return null;
+                    }
+                    else
+                    {
+                        return "Expected '}' but found end of file or invalid token.";
+                    }
+                }
+                else
+                {
+                    return "Expected '{' but found '" + (index < tokens.Count ? tokens[index] : "end of file") + "'.";
+                }
+            }
+
+            if (tokens.Count == 0)
+            {
+                return "No statements found.";
+            }
+
+            while (index < tokens.Count)
+            {
+                var error = ParseStatement();
+                if (!string.IsNullOrEmpty(error)) return error;
+            }
+
+            return null;
+        }
+
+    }
+}
